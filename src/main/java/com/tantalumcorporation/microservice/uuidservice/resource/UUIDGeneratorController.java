12cc92f080ef9fc33cc5c3bec6c875c0e74e4c71
motolola <motolola@icloud.com>
2018-09-20 09:35:31 +0100

    Clean up

--- src/main/java/com/tantalumcorporation/microservice/uuidservice/resource/UUIDGeneratorController.java
+++ src/main/java/com/tantalumcorporation/microservice/uuidservice/resource/UUIDGeneratorController.java
@@ -2,7 +2,6 @@ package com.tantalumcorporation.microservice.uuidservice.resource;
 
 import com.tantalumcorporation.microservice.uuidservice.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
